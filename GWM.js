b324e82de6f5c9df352a7e2a29f7f876ce112b8b
Ricardo Salcido <rsalcido@liberty.edu>
2019-02-04 16:56:57 -0600

    Week5

---
+++ GWM.js
@@ -0,0 +1,23 @@
+// var h1ID = document.getElementById("first");
+// console.log(h1ID);
+/////////// part a to number 1
+// var h1class = document.getElementsByClassName("special");
+// console.log(h1tag[0]);
+// // part b to number
+// var h1tag= document.getElementsByTagName('p');
+// console.log(h1tag[0]);
+////// part c to number1
+
+////number 2
+//     var stags= document.querySelector("special");
+//   console.log(stags);
+//// part 2 to number 2
+//     var  selec= document.querySelectorAll('special');
+//     console.log(selec);
+
+/// number 3
+// var color = document.getElementsByTagName('h1')[0];
+//     color.style.color="#634aa9";
+////// number 4
+var color = document.getElementsByTagName('p' )[3];
+    color.style.color='#fc0';
